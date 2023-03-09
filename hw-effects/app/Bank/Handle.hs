639aa1bdb0571b4c3c6dc680b477047391e7a3a4
sashkent3 <33320473+sashkent3@users.noreply.github.com>
2023-03-09 23:05:20 +0200

    migrate from IOArray to IORef

--- hw-effects/app/Bank/Handle.hs
+++ hw-effects/app/Bank/Handle.hs
@@ -1,13 +1,15 @@
 module Bank.Handle where
 
-import Data.Array.IO (IOArray)
+import Data.IORef (IORef)
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
 import Numeric.Natural (Natural)
 
 type Account = Int
 
 type Balance = Natural
 
-newtype BankHandle = BankHandle {getBankStorage :: IOArray Account Balance}
+newtype BankHandle = BankHandle {getBankStorage :: IORef (IntMap Balance)}
 
 -- (1.5 балла) реализуйте интерфейс @BankHandle@. Не забудьте, что для какой-то
 -- монады должен быть реализован инстанс @MonadBank@.
