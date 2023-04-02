7e4924b94de21016cda934fdb5a4d3935ae96943
ayvarchello <sayvar@list.ru>
2023-04-02 22:11:56 +0300

    Done

--- hw-spreadsheet/src/Data/Spreadsheet.hs
+++ hw-spreadsheet/src/Data/Spreadsheet.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Data.Spreadsheet where
 
-import Control.Monad.Except (MonadError)
-import Control.Monad.Free (Free)
-import Data.Array (Array)
+import Control.Monad.Except (MonadError(throwError))
+import Control.Monad.Free (Free(Free, Pure))
+import Data.Array (Array, assocs, bounds, array, (!))
+import qualified Text.Tabl as TT
+import qualified Data.Text as T
+import Data.Functor.Fix(ffix)
 
 -- | A spreadsheet cell can be indexed by pair of @Int@s.
 type Index = (Int, Int)
@@ -16,25 +23,51 @@ newtype Spreadsheet a = Spreadsheet
   }
   deriving (Functor)
 
+emptyArr :: Array Index (Maybe a)
+emptyArr = array ((0, 0), (0, 0)) []
+
+emptySS :: Spreadsheet a
+emptySS = Spreadsheet emptyArr
+
+
 -- (1 балл) Сделайте @Spreadsheet a@ представителем класса @Show@.
 -- Чтобы вывести двумерную таблицу с выравниванием, можно воспользоваться
--- арсеналом библиотеки @pretty@.
+-- арсеналом библиотеки @pretty@
 
-instance Show a => Show (Spreadsheet a)
+instance Show a => Show (Spreadsheet a) where
+  show :: Show a => Spreadsheet a -> String
+  show a = 
+    let it ((rowIndex, _), elem) (currentRowIndex, chunks@(chunkHead: chunkTail)) = 
+            if maybe True (rowIndex == ) currentRowIndex then 
+              (Just rowIndex, (elem : chunkHead) : chunkTail) else 
+              (Just rowIndex, [elem] : chunks) in
+    let tData = snd $ foldr (it . (\(index, element) -> (index, T.pack $ maybe "*" show element))) (Nothing, [[]]) $ assocs $ getSpreadsheet a in
+      T.unpack $ TT.tabl TT.EnvAscii (TT.DecorAll) (TT.DecorAll) (repeat TT.AlignLeft) tData
 
 -- (1 балл) Напишите вспомогательные функции для манипуляции таблицами.
 
 -- | Appends new, empty row to the bottom of a spreadsheet
 appendRow :: Spreadsheet a -> Spreadsheet a
-appendRow = error "appendRow is not defined"
+appendRow sheet = 
+  let arr = getSpreadsheet sheet
+      ((top, left), (bottom, right)) = bounds arr
+      newRow = [((bottom + 1, j), Nothing) | j <- [left..right]]
+      newBounds = ((top, left), (bottom + 1, right))
+  in Spreadsheet $ array newBounds $ assocs arr ++ newRow
 
 -- | Appends new, empty column to the right of a spreadsheet
+-- | Adds a new column to the right of the spreadsheet.
 appendCol :: Spreadsheet a -> Spreadsheet a
-appendCol = error "appendCol is not defined"
+appendCol sheet =
+    let arr = getSpreadsheet sheet
+        ((top, left), (bottom, right)) = bounds arr
+        newColumn = [((i, right + 1), Nothing) | i <- [top..bottom]]
+        newBounds = ((top, left), (bottom, right + 1))
+    in Spreadsheet $ array newBounds $ assocs arr ++ newColumn
 
 -- | Returns cell in a spreadsheet by index
 (!?) :: Spreadsheet a -> Index -> Maybe a
-(!?) = error "spreadsheet index is not defined"
+(!?) (Spreadsheet sheet) = (sheet !)
 
 -- | A @Formula f a@ in a spreadsheet cell can:
 --
@@ -63,17 +96,17 @@ data EvalError
 -- параметра типа @Integer@. Для этого есть готовый комбинатор в модуле
 -- @Control.Monad.Free@, найдите его.
 
--- | A function to compute values in a spreadsheet.
-calc ::
+calc :: forall f m a.
   (Traversable f, MonadError EvalError m) =>
-  -- | How to perform operations listed in grammar @f@.
+  -- | The function that performs operations on the given `Traversable` function `f`.
   (f (m a) -> m a) ->
-  -- | Recursion limit.
+  -- | The recursion limit.
   Integer ->
-  -- | Spreadsheet of formulae.
+  -- | The spreadsheet of formulae.
   Spreadsheet (Formula f a) ->
+  -- | The spreadsheet of computed expressions.
   Spreadsheet (m a)
-calc alg limit = error "calc is not defined"
+calc alg limit = fmap (evalExpression alg limit) . ffix . fmap (flip prepareExpression)
   where
     evalExpression ::
       (Traversable f, MonadError EvalError m) =>
@@ -81,7 +114,10 @@ calc alg limit = error "calc is not defined"
       Integer ->
       Free f (m a) ->
       m a
-    evalExpression alg limit = error "evalExpression is not defined"
+    evalExpression _ _ (Pure x) = x
+    evalExpression alg limit (Free v)
+      | limit == 0 = throwError RecursionLimitExceeded
+      | otherwise = alg $ (evalExpression alg (limit - 1)) <$> v
 
     prepareExpression ::
       (Functor f, MonadError EvalError m) =>
@@ -92,4 +128,8 @@ calc alg limit = error "calc is not defined"
       Free f (m a)
     -- \^ How do you substitute expressions in @f@
     -- to create another expression?
-    prepareExpression s f = error "prepareExpression is not defined"
+    prepareExpression s f = f >>= \case 
+                                    (Left idx) -> case s !? idx of 
+                                      Nothing -> Pure $ throwError BlankReferee
+                                      Just formula -> formula 
+                                    (Right const) -> Pure $ pure const
--- hw-spreadsheet/src/Handle/Spreadsheet.hs
+++ hw-spreadsheet/src/Handle/Spreadsheet.hs
@@ -5,12 +5,18 @@
 module Handle.Spreadsheet where
 
 import Control.Monad.Except (MonadError)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Reader (MonadReader)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (MonadReader, asks)
 import Data.Array.IO (IOArray)
-import Data.IORef (IORef)
+import Data.IORef (IORef, newIORef, readIORef)
 import Data.Spreadsheet (EvalError, Formula, Index, Spreadsheet)
+import qualified Data.Spreadsheet as P
+import qualified Data.Array as A
 import qualified Data.Spreadsheet as S
+import qualified Data.Text as T
+import GHC.IOArray (newIOArray, writeIOArray, readIOArray, boundsIOArray)
+import Control.Arrow (Arrow(arr))
+import Data.Foldable (for_)
 
 type Value = Rational
 
@@ -26,31 +32,59 @@ instance HasSpreadsheet f (SpreadsheetHandle f) where
   spreadsheet = id
 
 -- (1 балл) Реализуйте интерфейс расширяемой таблицы.
+  
+readRaw :: IOArray Index (Maybe (Cell f)) -> IO (Spreadsheet (Cell f))
+readRaw arr = P.Spreadsheet . A.listArray bounds <$> traverse (readIOArray arr) indices
+  where
+    bounds@((top, bottom), (left, right)) = boundsIOArray arr
+    indices = [(i, j) | i <- [top..bottom], j <- [left..right]]
 
 newSpreadsheetHandle :: Spreadsheet (Cell f) -> IO (SpreadsheetHandle f)
-newSpreadsheetHandle = error "newSpreadsheetHandle is not defined"
+newSpreadsheetHandle sheet = do
+  let bnds = A.bounds $ P.getSpreadsheet sheet
+  arr <- newIOArray (bnds) Nothing 
+  ref <- newIORef arr
+  return $ SpreadsheetHandle ref
 
 readCell ::
   (HasSpreadsheet f e, MonadReader e m, MonadIO m) =>
   Index ->
   m (Maybe (Cell f))
-readCell index = error "readCell is not defined"
+readCell index = do
+  sh <- asks (getSpreadsheetHandle . spreadsheet)
+  liftIO $ readIORef sh >>= \arr -> readIOArray arr index
 
 writeCell ::
   (HasSpreadsheet f e, MonadReader e m, MonadIO m) =>
   Index ->
   Maybe (Cell f) ->
   m ()
-writeCell index cell = error "writeCell is not defined"
+writeCell index cell = do
+  sh <- asks (getSpreadsheetHandle . spreadsheet)
+  liftIO $ readIORef sh >>= \arr -> writeIOArray arr index cell
 
 bounds :: (HasSpreadsheet f e, MonadReader e m, MonadIO m) => m (Index, Index)
-bounds = error "bounds is not defined"
+bounds = do
+  sh <- asks (getSpreadsheetHandle . spreadsheet)
+  arr <- liftIO $ readIORef sh
+  pure $ boundsIOArray arr
 
 appendRow :: (HasSpreadsheet f e, MonadReader e m, MonadIO m) => m ()
-appendRow = error "appendRow is not defined"
+appendRow = do
+  spreadsheetHandle <- asks (getSpreadsheetHandle . spreadsheet)
+  spreadsheetArray <- liftIO $ readIORef spreadsheetHandle
+  spreadsheetData <- liftIO $ readRaw spreadsheetArray
+  let newSpreadsheetData = P.getSpreadsheet (P.appendCol spreadsheetData)
+  liftIO $ for_ (A.assocs newSpreadsheetData) $ \(idx, val) -> writeIOArray spreadsheetArray idx val
 
 appendCol :: (HasSpreadsheet f e, MonadReader e m, MonadIO m) => m ()
-appendCol = error "appendCol is not defined"
+appendCol = do
+  spreadsheetHandle <- asks (getSpreadsheetHandle . spreadsheet)
+  spreadsheetArray <- liftIO $ readIORef spreadsheetHandle
+  spreadsheetData <- liftIO $ readRaw spreadsheetArray
+  let newSpreadsheetData = P.appendCol spreadsheetData
+  liftIO $ (for_ (A.assocs $ P.getSpreadsheet newSpreadsheetData) (uncurry $ writeIOArray spreadsheetArray))
+
 
 calcSpreadsheet ::
   ( HasSpreadsheet f e,
@@ -62,4 +96,8 @@ calcSpreadsheet ::
   (f (n Value) -> n Value) ->
   Integer ->
   m (Spreadsheet (n Value))
-calcSpreadsheet alg limit = error "calcSpreadsheet is not defined"
+calcSpreadsheet alg limit = do
+  handle <- asks (getSpreadsheetHandle . spreadsheet)
+  arr <- liftIO $ readIORef handle
+  ss <- liftIO $ readRaw arr
+  pure $ P.calc alg limit ss 
